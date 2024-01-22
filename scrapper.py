a2b18a2f739c4491f1742e6566f8fde9476da1c9
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2024-01-22 23:16:22 +0530

    scrape table data
    scrapes result data of every race for every year

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 
 from bs4 import BeautifulSoup
 import requests
@@ -14,5 +15,19 @@ for season in seasons:
     season_request = requests.get(season_url)
     season_soup = BeautifulSoup(season_request.content, 'lxml')
 
-    season_sections = season_soup.find("div", class_="section")
-    
+    season_section = season_soup.select_one("div.section h3:contains('schedule') + table")
+    races = season_section.find_all("a")
+
+    for race in races:
+        race_url = homepage + race["href"]
+        race_request = requests.get(race_url)
+        race_soup = BeautifulSoup(race_request.content, 'lxml')
+
+        table_results = race_soup.select_one("div#race-results table")
+        # print(table_results)
+        #
+        df = pd.read_html(table_results)[0]
+        #
+        # df.to_csv(f"{race_url}.csv", index=False)
+
+    break
