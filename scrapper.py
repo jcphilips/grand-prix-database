d939e9a27421cc47ff5946ebd52d46276e209c52
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2024-01-22 22:36:43 +0530

    update scrapper to use a different source

--- scrapper.py
+++ scrapper.py
@@ -1,33 +1,18 @@
-from bs4 import BeautifulSoup
-import requests
 import re
 
-url = requests.get("https://en.wikipedia.org/wiki/List_of_Formula_One_Grands_Prix")
+from bs4 import BeautifulSoup
+import requests
 
-soup = BeautifulSoup(url.content, "lxml")
+homepage = "https://pitwall.app"
+homepage_request = requests.get(homepage)
 
-def navboxes(soup):
+soup = BeautifulSoup(homepage_request.content, "lxml")
 
-    navboxes = soup.find_all("table", class_="navbox-inner")
+seasons = soup.find("div", class_="footer-block-full").find_all("a")
+for season in seasons:
+    season_url = homepage + season["href"]
+    season_request = requests.get(season_url)
+    season_soup = BeautifulSoup(season_request.content, 'lxml')
 
-    for navbox in navboxes:
-        year_links = navbox.find_all("a", href=lambda href: href and href.startswith('/wiki/'), title=lambda title: re.match(r'^.*(?<!\bseason\b)(?!2024)\b\d{4}\b.*$', title))
-
-        for year_link in year_links:
-            year_url = "https://en.wikipedia.org" + year_link["href"]
-            print(year_url)
-            year_response = requests.get(year_url)
-            year_soup = BeautifulSoup(year_response.content, 'lxml')
-
-            race_element = year_soup.find_all("table")
-#             for table in tables:
-#                 process_table(table, year_link[title])
-#
-#
-# def process_table(table, year):
-#     with open(f"{year}.csv", 'w'):
-#         rows = table.find_all("tr")
-#         for row in rows:
-#             cells = row.find_all("td")
-#             print(cells)
-navboxes(soup)
+    season_sections = season_soup.find("div", class_="section")
+    
